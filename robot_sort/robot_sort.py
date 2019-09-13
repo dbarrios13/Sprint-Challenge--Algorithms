627d8d4035fa006dc5f0a66503c3e467986869e3
David Barrios <David Barrios>
2019-09-13 12:33:57 -0700

    finish recursive count

--- robot_sort/robot_sort.py
+++ robot_sort/robot_sort.py
@@ -96,8 +96,43 @@ class SortingRobot:
         """
         Sort the robot's list.
         """
-        # Fill this out
-        pass
+        for i in range(0, len(self._list)):
+            if self.can_move_right():
+                if self.compare_item() == None or self.compare_item() == -1:
+                    self.swap_item()
+                    self.move_right()                
+                else: 
+                    self.move_right()    
+            if i == len(self._list) - 1:
+                self.set_light_on()
+                while self.light_is_on():                     
+                    if self.can_move_left():
+                        if self.compare_item() == None or self.compare_item() == 1:
+                            self.swap_item()
+                            self.move_left()
+                        else: 
+                            self.move_left()  
+                    else:
+                        self.swap_item()
+                        self.set_light_off()
+
+        # 
+        # while self.light_is_on():
+        #     if self.can_move_right() and :
+        #         if self.compare_item() == None or self.compare_item() == -1:
+        #             self.swap_item()
+        #             self.move_right()                
+        #         else: 
+        #             self.move_right()                     
+        #     elif self.can_move_left():
+        #         if self.compare_item() == None or self.compare_item() == 1:
+        #             self.swap_item()
+        #             self.move_left()
+        #         else: 
+        #             self.move_left()                                   
+                
+
+        
 
 
 if __name__ == "__main__":
